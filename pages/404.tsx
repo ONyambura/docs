39d7ee6e887a18c7daf0a1efcd2ef4ed1fd6cdc0
ONyambura <51477970+ONyambura@users.noreply.github.com>
2025-11-25 21:54:59 +0200

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -12,7 +12,7 @@ const FourOhFour: React.FC = () => {
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-center bg-neutral-900">
-      <img src="https://raw.githubusercontent.com/ONyambura/pictures/refs/heads/main/N%20Logos%20-%20Purple/logo.png" width="64" />
+      <img src="https://github.com/ONyambura/pictures/blob/main/N%20Logos%20-%20Purple/new_logo_nobg.png" width="64" />
       <p className="text-4xl text-white">Woopsss 404</p>
       <p className="text-lg text-white">Moving back to home...</p>
     </div>
